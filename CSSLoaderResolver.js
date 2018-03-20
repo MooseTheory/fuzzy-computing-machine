7510b7d2f737755199bba21584685e0086f276d2
David Merris <david.a.merris@jpmorgan.com>
2018-03-20 17:12:29 -0400

    Fixed the MDC Issue

--- CSSLoaderResolver.js
+++ CSSLoaderResolver.js
@@ -6,8 +6,8 @@
  * Modified by Yuxi Evan You
  */
 
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const path = require('path')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require("path");
 
 module.exports = class CSSLoaderResolver {
   /**
@@ -20,7 +20,7 @@ module.exports = class CSSLoaderResolver {
    * @param {boolean} [options.postcss=undefined] Enable postcss-loader.
    * @param {Object} [options.loaderOptions={}] Options to pass on to loaders.
    */
-  constructor ({
+  constructor({
     sourceMap,
     modules,
     localIdentName,
@@ -29,111 +29,118 @@ module.exports = class CSSLoaderResolver {
     postcss,
     loaderOptions
   } = {}) {
-    this.cssLoader = 'css-loader'
-    this.fallbackLoader = 'vue-style-loader'
-    this.sourceMap = sourceMap
-    this.extract = extract && !process.env.VUE_CLI_CSS_SHADOW_MODE
-    this.minimize = minimize
-    this.modules = modules
-    this.localIdentName = localIdentName
-    this.postcss = postcss
-    this.loaderOptions = loaderOptions || {}
+    this.cssLoader = "css-loader";
+    this.fallbackLoader = "vue-style-loader";
+    this.sourceMap = sourceMap;
+    this.extract = extract && !process.env.VUE_CLI_CSS_SHADOW_MODE;
+    this.minimize = minimize;
+    this.modules = modules;
+    this.localIdentName = localIdentName;
+    this.postcss = postcss;
+    this.loaderOptions = loaderOptions || {};
   }
 
-  getLoader (test, loader, options = {}) {
+  getLoader(test, loader, options = {}) {
     const cssLoaderOptions = {
       sourceMap: this.sourceMap,
       minimize: this.minimize
-    }
+    };
 
     if (this.modules) {
-      cssLoaderOptions.modules = true
-      cssLoaderOptions.importLoaders = 1
-      cssLoaderOptions.localIdentName = this.localIdentName
+      cssLoaderOptions.modules = true;
+      cssLoaderOptions.importLoaders = 1;
+      cssLoaderOptions.localIdentName = this.localIdentName;
     }
 
-    if (loader === 'css') {
-      Object.assign(cssLoaderOptions, options)
+    if (loader === "css") {
+      Object.assign(cssLoaderOptions, options);
     }
 
-    const use = [{
-      loader: this.cssLoader,
-      options: cssLoaderOptions
-    }]
+    const use = [
+      {
+        loader: this.cssLoader,
+        options: cssLoaderOptions
+      }
+    ];
 
-    if (loader !== 'postcss' && this.postcss !== false) {
+    if (loader !== "postcss" && this.postcss !== false) {
       use.push({
-        loader: 'postcss-loader',
+        loader: "postcss-loader",
         options: {
           sourceMap: this.sourceMap
         }
-      })
+      });
     }
 
-    if (loader && loader !== 'css') {
+    if (loader && loader !== "css") {
       use.push({
-        loader: loader + '-loader',
+        loader: loader + "-loader",
         options: Object.assign({}, this.loaderOptions[loader] || {}, options, {
           sourceMap: this.sourceMap
         })
-      })
+      });
     }
 
     return {
       test,
-      use: this.extract ? ExtractTextPlugin.extract({
-        use,
-        fallback: this.fallbackLoader
-      }) : [{
-        loader: this.fallbackLoader,
-        options: {
-          shadowMode: !!process.env.VUE_CLI_CSS_SHADOW_MODE,
-          sourceMap: this.sourceMap
-        }
-      }, ...use]
-    }
+      use: this.extract
+        ? ExtractTextPlugin.extract({
+            use,
+            fallback: this.fallbackLoader
+          })
+        : [
+            {
+              loader: this.fallbackLoader,
+              options: {
+                shadowMode: !!process.env.VUE_CLI_CSS_SHADOW_MODE,
+                sourceMap: this.sourceMap
+              }
+            },
+            ...use
+          ]
+    };
   }
 
-  css (test = /\.css$/) {
-    return this.getLoader(test, 'css')
+  css(test = /\.css$/) {
+    return this.getLoader(test, "css");
   }
 
-  sass (test = /\.sass$/) {
-    return this.getLoader(test, 'sass', {
-      includePaths: [path.resolve(__dirname, '..', '..', '..', '..')],
+  sass(test = /\.sass$/) {
+    return this.getLoader(test, "sass", {
+      includePaths: [path.resolve(__dirname, "..", "..", "..", "..")],
       indentedSyntax: true
-    })
+    });
   }
 
-  scss (test = /\.scss$/) {
-    return this.getLoader(test, 'sass', {
-      includePaths: [path.resolve(__dirname, '..', '..', '..', '..')]
-    })
+  scss(test = /\.scss$/) {
+    return this.getLoader(test, "sass", {
+      includePaths: [path.resolve(__dirname, "..", "..", "..", "..")]
+    });
   }
 
-  less (test = /\.less$/) {
-    return this.getLoader(test, 'less')
+  less(test = /\.less$/) {
+    return this.getLoader(test, "less");
   }
 
-  styl (test = /\.styl$/) {
-    return this.getLoader(test, 'stylus')
+  styl(test = /\.styl$/) {
+    return this.getLoader(test, "stylus");
   }
 
-  stylus (test = /\.stylus$/) {
-    return this.getLoader(test, 'stylus')
+  stylus(test = /\.stylus$/) {
+    return this.getLoader(test, "stylus");
   }
 
-  vue () {
-    const originalPostcss = this.postcss
-    const originalModules = this.modules
-    this.postcss = false
-    this.modules = false
-    const loaders = {}
-    for (const lang of ['css', 'sass', 'scss', 'less', 'stylus', 'styl']) {
-      loaders[lang] = this[lang]().use
+  vue() {
+    const originalPostcss = this.postcss;
+    const originalModules = this.modules;
+    this.postcss = false;
+    this.modules = false;
+    const loaders = {};
+    for (const lang of ["css", "sass", "scss", "less", "stylus", "styl"]) {
+      loaders[lang] = this[lang]().use;
     }
-    this.postcss = originalPostcss
-    this.modules = originalModules
-    return loaders
+    this.postcss = originalPostcss;
+    this.modules = originalModules;
+    return loaders;
   }
-}
+};
