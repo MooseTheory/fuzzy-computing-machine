315a061bc16a474afa3c4852e776ff83ebd5e1f3
David Merris <me@davidmerris.com>
2018-03-25 13:24:29 -0400

    Finally Figured Out the Webpack Chain Config

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,19 @@
 /* eslint-env node */
+const path = require("path");
+
+module.exports = {
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap(options => {
+        var scssLoaderIndex = options.loaders.scss.findIndex(function(e) {
+          return e.loader === "sass-loader";
+        });
+        options.loaders.scss[scssLoaderIndex].options.includePaths = [
+          path.resolve(__dirname, "node_modules")
+        ];
+        return options;
+      });
+  }
+};
